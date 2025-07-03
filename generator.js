51b01f6eff019af7fb1b9b915fd1efe793eb15bc
Hazrat-Ali9 <programmer.hazratali@gmail.com>
2025-07-03 20:06:28 +0600

    genarator js  :alien:

--- generator.js
+++ generator.js
@@ -1,5 +1,5 @@
 let previousPassword = '';
-
+// Genarator ts
 document.getElementById('generate').addEventListener('click', function() {
     var length = document.getElementById('length').value;
     var useUppercase = document.getElementById('uppercase').checked;
